d9c65bf66d1571eb84b93f9e7edcbd943f8ac071
“Yu” <yuqian.124@gmail.com>
2025-09-16 11:21:22 +0800

    feat: migrate styles from CSS to SCSS and update imports accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,16 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
     <HashRouter>
-      <div className='App w-full h-full'>
-        <div className="app-container w-full h-full overflow-hidden">
-          <Routes>
-            <Route path="/" element={<AIAssistant />} />
-            <Route path="/filebox" element={<FileBox />} />
-            <Route path="/menu" element={<AssistantMenu />} />
-            <Route path="/settings" element={<SettingsPanel />} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/recycle" element={<RecycleBinPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
+      <div className="w-full h-full overflow-hidden">
+        <Routes>
+          <Route path="/" element={<AIAssistant />} />
+          <Route path="/filebox" element={<FileBox />} />
+          <Route path="/menu" element={<AssistantMenu />} />
+          <Route path="/settings" element={<SettingsPanel />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/recycle" element={<RecycleBinPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
     </HashRouter>
   )
