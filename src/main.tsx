b39691c64591dcd231cda50b07e9d1a7a47cc1e7
“Yu” <yuqian.124@gmail.com>
2025-10-15 11:40:29 +0800

    feat: relocate SpritePlayerContext to AIAssistant component directory and update imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { SpritePlayerProvider } from '@/context/SpritePlayerContext'
+import { SpritePlayerProvider } from '@/components/AIAssistant/context/SpritePlayerContext'
 
 import './index.scss'
 
